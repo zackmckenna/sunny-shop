3fa2a7252708ebdc92f40c3d561a1c032b04e83c
Zack McKenna <zackmckenna@gmail.com>
2020-06-02 16:00:27 -0400

    created app copy for reference

--- src/App copy.js
+++ src/App copy.js
@@ -37,6 +37,10 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
           <LambdaDemo />
         </header>
       </div>
