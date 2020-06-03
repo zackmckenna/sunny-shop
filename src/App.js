2409ec5e3fb94c38cffdd4e21d3b3d1aec27ce6f
Zack McKenna <zackmckenna@gmail.com>
2020-06-03 13:06:44 -0400

    tailwind

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react"
 import logo from "./logo.svg"
 import "./App.css"
+import SunnyNav from './components/SunnyNav/SunnyNav'
+import Home from './components/Home/Home'
 
 class LambdaDemo extends Component {
   constructor(props) {
@@ -35,10 +37,11 @@ class LambdaDemo extends Component {
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <LambdaDemo />
+      <div className='App-container'>
+        <header>
+          {/* <SunnyNav /> */}
         </header>
+        <Home />
       </div>
     )
   }
---
+++ src/components/Home/Home.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Container, Row, Col } from 'react-bootstrap'
+
+const Home = () => {
+  return (
+    <>
+      <Container>
+        <Row>
+          <Col style={{color: '#313638', textAlign: 'center', backgroundColor: "#E4B363", padding: '1em 1em', borderRadius: '2px', fontSize: '2em' }}>a sunny sunday</Col>
+        </Row>
+      </Container>
+    </>
+  )
+}
+
+export default Home;
