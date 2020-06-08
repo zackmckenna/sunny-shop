875eb43e1625aa9e04e70d72105412536db1fd65
Zack McKenna <zackmckenna@gmail.com>
2020-06-08 15:25:44 -0400

    added jewley pic

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,13 +1,19 @@
 import React from 'react'
 import { Container, Row, Col, Image } from 'react-bootstrap'
 import emilyPic from '../../pictures/em-2.jpg'
+import jewleryPic from '../../pictures/ss-1.jpg'
+
+import './Home.css'
 
 const Home = () => {
   return (
     <>
-      <Container className='mt-4 text-center'>
+      <Container className='home-container text-center'>
         <Row className='align-items-center'>
-          <Col><h1>Hey, I'm Emily</h1></Col><Col><Image rounded fluid src={emilyPic}></Image></Col>
+          <Col><h1>Hey, I'm Emily</h1></Col><Col><Image className='emilyPic' rounded fluid src={emilyPic}></Image></Col>
+        </Row>
+        <Row>
+          <Col><Image  className='jewleryPic' rounded fluid  src={jewleryPic}></Image></Col>
         </Row>
       </Container>
     </>
