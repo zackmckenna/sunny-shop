ecb9371c1e0f39b5cc2f632acacf7450815d0693
Zack McKenna <zackmckenna@gmail.com>
2020-06-09 13:31:53 -0400

    added responsive mobile design and squooshed images

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -4,21 +4,25 @@ import emilyPic from '../../pictures/em-2.jpg'
 import jewleryPic from '../../pictures/ss-1.jpg'
 import jewleryPic2 from '../../pictures/ss-3.jpg'
 import jewleryPic3 from '../../pictures/ss-4.jpg'
-
+import { faCircle } from '@fortawesome/free-solid-svg-icons'
 import './Home.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 const Home = () => {
   return (
     <>
       <Container fluid className='home-container text-center'>
         <Row className='home-section align-items-center'>
-          <Col className='home-section-text'><h1>Hey, I'm Emily</h1></Col><Col><Image className='emilyPic' rounded fluid src={emilyPic}></Image></Col>
+          <div style={{position:'relative'}}>
+            <FontAwesomeIcon className="fa" icon={faCircle}/>
+          </div>
+          <Col sm className='home-section-text'><h1>Hey, I'm Emily</h1></Col><Col><Image className='emilyPic' rounded fluid src={emilyPic}></Image></Col>
         </Row>
         <Row className='home-section align-items-center'>
-          <Col><Image  className='jewleryPic' rounded fluid  src={jewleryPic}></Image></Col><Col className='home-section-text'><h2>and I make awesome jewlery</h2></Col>
+          <Col sm><Image  className='jewleryPic' rounded fluid  src={jewleryPic}></Image></Col><Col xs className='home-section-text'><h2>and I make awesome jewlery</h2></Col>
         </Row>
         <Row className='home-section align-items-center'>
-          <Col className='text-center'>
+          <Col sm className='text-center'>
             {/* <h3>check it out</h3> */}
           </Col>
         </Row>
--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import './Logo.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSun } from '@fortawesome/free-solid-svg-icons'
+
 
 const Logo = () => {
   return (
     <div className='logo-div'>
-      <div className='circle'>
-
-      </div>
       <h1 className='logo-text sunny'>sunny</h1>
       <h1 className='logo-text sunday'>sunday</h1>
     </div>
